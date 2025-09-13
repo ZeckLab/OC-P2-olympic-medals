df32a48129f49697b69dc31f5cf103a42bc87382
ZeckLab <sturbout@gmail.com>
2025-09-13 22:49:22 +0200

    Step 2 completed: data fetched and TypeScript interfaces defined

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { OlympicService } from './core/services/olympic.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
+
 export class AppComponent implements OnInit {
   constructor(private olympicService: OlympicService) {}
 
--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -1,26 +1,29 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, of } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
+import { Olympic } from '../models/Olympic';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OlympicService {
   private olympicUrl = './assets/mock/olympic.json';
-  private olympics$ = new BehaviorSubject<any>(undefined);
+  // Holds Olympic data fetched from API. Null = not loaded or error.
+  private olympics$ = new BehaviorSubject<Olympic[] |null>(null);
 
   constructor(private http: HttpClient) {}
 
   loadInitialData() {
-    return this.http.get<any>(this.olympicUrl).pipe(
+    return this.http.get<Olympic[]>(this.olympicUrl).pipe(
       tap((value) => this.olympics$.next(value)),
       catchError((error, caught) => {
         // TODO: improve error handling
-        console.error(error);
+        console.error("An error occurred while loading Olympic data.", error);
         // can be useful to end loading state and let the user know something went wrong
+        // gestion simple de l'erreur, fault-il un retry ?
         this.olympics$.next(null);
-        return caught;
+        return of(null);
       })
     );
   }
